aea9cd204953b584ffb61e2c95df962194aee326
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-22 21:46:49 -0500

    commit tree a5c1b55b65bf4178decab8f8e22d4fabcdb179e2

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+const headquarter = 42;
+function distanceFromHqInBlocks(pickUp) {
+    let distance
+    let dist = headquarter - pickUp;
+    if ( dist>= 0) {
+        distance = dist}
+        else {distance = -dist;}
+return distance;
+}
+
+const blockSize = 264;
+
+function distanceFromHqInFeet(pickUp) {
+    let blocks = distanceFromHqInBlocks(pickUp)
+    return blocks*blockSize;
+}
+
+function distanceTravelledInFeet(headquarter, pickUp) {
+    let distance
+    let dist = headquarter - pickUp;
+    if ( dist>= 0) {
+        distance = dist}
+        else {distance = -dist;}
+return distance*blockSize;
+}
+
+const pricePerFeet = 0.02;
+
+function calculatesFarePrice(headquarter, pickUp) {
+    let distance = Math.abs(headquarter - pickUp)*blockSize;
+    let reply 
+    switch (true) {
+        case (distance <= 400): 
+            reply = 0;
+            break;
+        case (distance >400 && distance <= 2000):
+            reply = (distance-400)*0.02;
+            break;
+        case (distance >2000 && distance < 2500):
+            reply = 25;
+            break;
+        case (distance >= 2500) : 
+            reply = 'cannot travel that far'
+             break;
+    }
+    return reply;
+}
